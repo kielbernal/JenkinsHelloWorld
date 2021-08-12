3962173462f05ef2062408523a827645d4111d16
kielbernal <85665573+kielbernal@users.noreply.github.com>
2021-08-12 16:33:37 +0800

    made some changes

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hello World, this is my first Jenkins project");
+		System.out.println("Hello World, made some changes");
 
 	}
 
